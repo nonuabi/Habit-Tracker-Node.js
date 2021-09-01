7d03133fab96af2b37351ff74d30117802df5141
nonuabi <nonuabi@gmail.com>
2021-09-01 19:52:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+//use express router
+app.use("/", require("./routes"));
+
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server ${err}`);
+    return;
+  }
+  console.log(`Server is running on port no ${port}`);
+});
