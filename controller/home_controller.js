b243cf88eedae5a04a3b0754d3c295dc5b58702f
nonuabi <nonuabi@gmail.com>
2021-09-02 00:34:37 +0530

    added controller and the router action

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,20 @@
+const Habit = require("../models/habit");
 module.exports.home = function (req, res) {
   return res.render("home", {
     title: "Home",
   });
+};
+
+module.exports.newHabit = function (req, res) {
+  console.log(req.body);
+
+  // const { content } = req.body;
+  // Habit.create({ content: content }, function (error, habit) {
+  //   if (error) {
+  //     console.log(`Error in creating new habit ${error}`);
+  //     return;
+  //   }
+  //   console.log(`new habit is created :: ${habit}`);
+  //   return res.redirect("back");
+  // });
 };
