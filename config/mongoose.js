e86f075134e2d401ba8d81ea1dbe77886ffc4af7
unknown <nonuabi@gmail.com>
2021-09-09 15:19:31 +0530

    update MongoseDB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,26 +1,33 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://localhost/habitTracker");
+mongoose.connect(
+  "mongodb+srv://abhishek:abhishek@cluster0.pacpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
-// db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-// db.once("open", function () {
-//   console.log("Connected to DataBase :: MongoDB");
-// });
+db.once("open", function () {
+  console.log("Connected to DataBase :: MongoDB");
+});
 
 // module.exports = db;
-const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://abhishek:abhishektest@cluster0.pacpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("Connected to DataBase :: MongoDB");
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient } = require("mongodb");
+// const uri =
+//   "mongodb+srv://abhishek:abhishektest@cluster0.pacpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   console.log("Connected to DataBase :: MongoDB");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// module.exports = {
+//   MongoURI:
+//     "mongodb+srv://Z4HMUo0JffOPhueM:Z4HMUo0JffOPhueM@cluster0.ov73y.mongodb.net/Z4HMUo0JffOPhueM?retryWrites=true&w=majority",
+// };
