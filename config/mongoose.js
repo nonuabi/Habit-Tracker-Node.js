7a9ed075f121cd3c3d93e7a11f1be8d79e3cb272
unknown <nonuabi@gmail.com>
2021-09-09 15:08:03 +0530

    connect mongoseDB atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,26 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/habitTracker");
+// mongoose.connect("mongodb://localhost/habitTracker");
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+// db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once("open", function () {
-  console.log("Connected to DataBase :: MongoDB");
+// db.once("open", function () {
+//   console.log("Connected to DataBase :: MongoDB");
+// });
+
+// module.exports = db;
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://abhishek:<password>@cluster0.pacpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-module.exports = db;
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("Connected to DataBase :: MongoDB");
+  // perform actions on the collection object
+  client.close();
+});
